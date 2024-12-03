554947f176c3a6a10b4a99781a41b6b876f09a55
zxu633 <zxu633@gatech.edu>
2024-12-03 02:20:51 -0500

    Update local_search.py

--- src/local_search.py
+++ src/local_search.py
@@ -14,7 +14,7 @@ def swap(route):
     res[i], res[j]=res[j], res[i]
     return res
 
-def solver(graph_mat, cutoff_time=900, rand_seed=None):
+def solver(graph_mat, cutoff_time, rand_seed):
     
     k=1.5
     M=1000
