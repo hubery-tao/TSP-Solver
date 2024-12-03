b872df73fa8a5d6020d033a70379f54e58f6f73e
zxu633 <zxu633@gatech.edu>
2024-12-03 02:17:16 -0500

    Update local_search.py

--- src/local_search.py
+++ src/local_search.py
@@ -1,1 +1,6 @@
-"""a heuristic algorithm with no guarantees, but that is effective in practice."""def solver(graph_mat, rand_seed):    return None, None
+"""
+a heuristic algorithm with no guarantees, but that is effective in practice.
+"""
+
+def solver(graph_mat, cutoff_time, rand_seed):
+    return None, None
